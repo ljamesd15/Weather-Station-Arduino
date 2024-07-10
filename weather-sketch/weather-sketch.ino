9c4338f2fb066e7fb58fbf6cd6b4878204e62ebd
James Davidson <james.davidson@jamesdavidson.info>
2024-07-10 06:35:12 -0700

    Fixed bug where MQTT disconnects. Opt to establish new connection every 5 min

--- weather-sketch/weather-sketch.ino
+++ weather-sketch/weather-sketch.ino
@@ -17,10 +17,12 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
+const unsigned long MAX_UNSIGNED_LONG = 4294967295;
 const int BACKOFF_IN_MILLIS = 5000;
-const long MILLIS_IN_SECONDS = 1000;
+const long MILLIS_IN_SECOND = 1000;
 const long SECONDS_IN_MINUTE = 60;
-const long DATA_INTERVAL_IN_MIN = 5;
+const double DATA_INTERVAL_IN_MIN = 5;
+const double DATA_INTERVAL_IN_MILLIS = MILLIS_IN_SECOND * SECONDS_IN_MINUTE * DATA_INTERVAL_IN_MIN;
 const int TIME_LEN = 24;
 
 const char ssid[] = NETWORK_SSID;
@@ -32,8 +34,8 @@ const int  port     = 1883;
 const char topic[]  = "weather";
 const char client_id[] = "arduino1";
 
-int count = 0;
-bool sentMessage = false;
+// To send weather data at the start
+unsigned long timeOfLastMessage = MAX_UNSIGNED_LONG - DATA_INTERVAL_IN_MILLIS;
 JsonDocument sensorMetadata;
 
 struct WeatherData {
@@ -58,12 +60,21 @@ time_t getTime() {
 
 void setup() {
   Serial.begin(9600);
-  if(!bmp280.init()){
+  Serial.println();
+  Serial.println();
+  Serial.println("Starting setup");
+  // Setup sensor metadata
+  sensorMetadata["sensorId"] = client_id;
+  sensorMetadata["location"] = "greenhouse";
+  sensorMetadata["tags"][0] = "test";
+
+  // Setup sensors
+  if (!bmp280.init()) {
     Serial.println("Device error!");
   }
   dht.begin(); // Initialize the DHT sensor
 
-  // Attempt to connect to Wifi network:
+  // Connect to the network
   Serial.print("Attempting to connect to WPA SSID: ");
   Serial.println(ssid);
   while (WiFi.begin(ssid, network_pass) != WL_CONNECTED) {
@@ -71,12 +82,67 @@ void setup() {
     Serial.print(".");
     delay(BACKOFF_IN_MILLIS);
   }
+  setSyncProvider(getTime);
+  setSyncInterval(300);
   Serial.println("You're connected to the network");
 
-  Serial.print("Attempting to connect to the MQTT broker: ");
-  Serial.print(broker);
-  Serial.print(":");
-  Serial.println(port);
+  Serial.println("Done with setup!");
+  Serial.println();
+};
+
+void loop() {
+  // call poll() regularly to allow the library to send MQTT keep alive which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
+  unsigned long currentTime = millis();
+  unsigned long timeSinceLastMessage;
+
+  if (currentTime < timeOfLastMessage) {
+    // Millis have looped back around
+    timeSinceLastMessage = (MAX_UNSIGNED_LONG - timeOfLastMessage) + currentTime;
+  } else {
+    timeSinceLastMessage = currentTime - timeOfLastMessage;
+  }
+  if (DEBUG) {
+    Serial.print("Now ");
+    Serial.println(currentTime);
+    Serial.print("Time of last message ");
+    Serial.println(timeOfLastMessage);
+    Serial.print("Time since last message ");
+    Serial.println(timeSinceLastMessage);
+  }
+
+  if (timeSinceLastMessage > DATA_INTERVAL_IN_MILLIS) {
+    bool connected = connectToBroker();
+    if (connected) {
+      WeatherData * weatherData = (WeatherData *) malloc (sizeof(struct WeatherData));
+      getWeatherData(weatherData);
+      setTime(weatherData);
+      sendData(weatherData);
+      free(weatherData);
+      timeOfLastMessage = currentTime;
+      mqttClient.stop();
+      if (DEBUG) {
+        Serial.print("Sent message at ");
+        Serial.println(timeOfLastMessage);
+      }
+    }
+  } else if (DEBUG) {
+    Serial.print("Skipping message because time since last message is ");
+    Serial.print(timeSinceLastMessage / MILLIS_IN_SECOND);
+    Serial.println(" seconds.");
+  }
+
+  delay(BACKOFF_IN_MILLIS);
+}
+
+bool connectToBroker() {
+  if (DEBUG) {
+    Serial.print("Attempting to connect to the MQTT broker: ");
+    Serial.print(broker);
+    Serial.print(":");
+    Serial.println(port);
+  }
 
   mqttClient.setId(client_id);
   mqttClient.setUsernamePassword(mqtt_username, mqtt_password);
@@ -85,38 +151,13 @@ void setup() {
     int connect_error = mqttClient.connectError();
     Serial.println(connect_error);
     Serial.println(connect_error < 0);
-
-    while (1);
+    return false;
   }
-  Serial.println("You're connected to the MQTT broker!");
-
-  sensorMetadata["sensorId"] = client_id;
-  sensorMetadata["location"] = "greenhouse";
-  sensorMetadata["tags"][0] = "test";
-
-  setSyncProvider(getTime);
-  setSyncInterval(300);
-  Serial.println("Done with setup!");
-  Serial.println();
-};
-
-void loop() {
-  if (!sentMessage) {
-    WeatherData * weatherData = (WeatherData *) malloc (sizeof(struct WeatherData));
-    getWeatherData(weatherData);
-    setTime(weatherData);
-    sendData(weatherData);
-    free(weatherData);
-    //sentMessage = true;
+  
+  if (DEBUG){
+    Serial.println("You're connected to the MQTT broker!");
   }
-
-  // Wait some time between measurements.
-  if (DEBUG) {
-    Serial.print("Waiting ");
-    Serial.print(MILLIS_IN_SECONDS * SECONDS_IN_MINUTE * DATA_INTERVAL_IN_MIN);
-    Serial.println(" milliseconds before sending more data");
-  }
-  delay(MILLIS_IN_SECONDS * SECONDS_IN_MINUTE * DATA_INTERVAL_IN_MIN);
+  return true;
 }
 
 void getWeatherData(WeatherData * data) {
@@ -125,17 +166,18 @@ void getWeatherData(WeatherData * data) {
   // check if any reads failed 
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
+  } else {
+    data->temperature = temperature;
+    data->humidity = humidity;
   }
-  data->temperature = temperature;
-  data->humidity = humidity;
   
   float pressure = bmp280.getPressure();          // In pascals
-  float bmpTemperature = bmp280.getTemperature(); // In celsius
   // check if any reads failed
-  if (isnan(pressure) || isnan(bmpTemperature)) {
+  if (isnan(pressure)) {
     Serial.println("Failed to read from BMP sensor!");
+  } else {
+    data->pressure = pressure * 0.01;                // Convert pascals to millibars
   }
-  data->pressure = pressure * 0.01;                // Convert pascals to millibars
 
   return data;
 }
@@ -147,10 +189,6 @@ void setTime(WeatherData * data) {
 }
 
 void sendData(WeatherData * data) {
-  // call poll() regularly to allow the library to send MQTT keep alive which
-  // avoids being disconnected by the broker
-  mqttClient.poll();
-
   JsonDocument weatherData;
   weatherData["sensorMetadata"] = sensorMetadata;
   weatherData["time"] = data->time;
